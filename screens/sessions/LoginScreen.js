377460b2477e1d58832ab48c203a8905901bf251
AmarinNon <nonprc123@hotmail.com>
2021-08-18 09:56:27 +0700

    add style

--- screens/sessions/LoginScreen.js
+++ screens/sessions/LoginScreen.js
@@ -1,17 +1,90 @@
 import React from 'react'
 import { View, Text } from 'react-native';
+import { useForm, Controller } from 'react-hook-form';
+import { TextInput, Button } from 'react-native-paper'
+import styles from './style';
+const LoginScreen = ({ navigation }) => {
+  
+  const { control, handleSubmit, formState: { errors } } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+  return (
+    <View style={styles.authFormContainer}>
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            style={styles.formInput}
+            label="Email"
+            mode="outlined"
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            value={value}
+          />
+        )}
+        name="email"
+        rules={{ required: true }}
+        defaultValue=""
+      />
+      <View style={styles.errorMsg}>
+        {errors.email && <Text style={styles.errorText}>You must fill in your email</Text>}
+      </View>
+      <Controller
+        control={control}
+        render={({ onChange, onBlur, value }) => (
+          <TextInput
+            style={styles.formInput}
+            label="Password"
+            mode="outlined"
+            secureTextEntry
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            value={value}
+          />
+        )}
+        name="password"
+        rules={{ required: true }}
+        defaultValue=""
+      />
+      <View style={styles.errorMsg}>
+        {errors.password && <Text style={styles.errorText}>You must fill in your password</Text>}
+      </View>
+      <View>
+        <Button
+          style={styles.submitButton}
+          mode="contained"
+          compact={false}
+          onPress={handleSubmit(onSubmit)}
+          icon="account-arrow-right"
+        >
+          Sign in
+        </Button>
 
-const LoginScreen = () => {
-    return(
-        <View>
-            <Text>LoginScreen</Text>
-            <Text> <h1>อัมรินทร์ น้อยเผ่า</h1></Text>
+      </View>
 
-            <Text><h1>11/08/2021</h1></Text>
-            </View>
-    )
-
+      <View style={styles.switchScreenText}>
+        <Text>
+          Don't have an account yet?
+        </Text>
+      </View>  
+      <Button
+        mode="outlined"
+        style={styles.switchBtn}
+        icon="account-plus"
+        compact
+        onPress={() => navigation.navigate('register')}>Register Account
+      </Button>
+      <p>Made by Amarin</p>
+    </View>
     
-};
+    
+  )
+  const LoginScreen = ({ navigation }) => {
+    
 
+
+  }
+  
+};
 export default LoginScreen;
